119d82771d3dd4931c5b49c51dc321c41e42e746
Sem <semik.kolesnikovv@gmail.com>
2022-07-06 15:40:19 +0200

    Initial Commit

---
+++ Memorize-Game/Memorize_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_GameApp.swift
+//  Memorize-Game
+//
+//  Created by Sem Koliesnikov on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
